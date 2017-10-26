8bd9f5bd94234b90a069b50d8270d085e0e30810
JenniferIzabel <izabeljennifer@gmail.com>
2017-10-26 17:24:39 -0200

    lista de plavras e dicas

---
+++ src/projetosd/ListaPalavrasDicas.java
@@ -0,0 +1,46 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package projetosd;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author jennifer
+ */
+public class ListaPalavrasDicas {
+    PalavraDica lista[] = {
+        new PalavraDica("hinoceronte", "animal"),
+        new PalavraDica("cavalo", "animal"),
+        new PalavraDica("tartaruga", "animal"),
+        new PalavraDica("raposa", "animal"),
+        new PalavraDica("boizinho", "pequeno animal"),
+        new PalavraDica("cachorrao", "grande animal"),
+        new PalavraDica("jacare", "reptil"),
+        new PalavraDica("morango", "fruta"),
+        new PalavraDica("melancia", "fruta"),
+        new PalavraDica("maracuja", "fruta"),
+        new PalavraDica("carambola", "fruta"),
+        new PalavraDica("abacaxi", "fruta"),
+        new PalavraDica("felicidade", "sentimento"),
+        new PalavraDica("tristeza", "sentimento"),
+        new PalavraDica("alegria", "sentimento"),
+        new PalavraDica("raiva", "sentimento"),
+        new PalavraDica("piloto", "profissao"),
+        new PalavraDica("professor", "profissao"),
+        new PalavraDica("jornalista", "profissao"),
+        new PalavraDica("jogador", "profissao"),
+        new PalavraDica("dinossauro", "reptil pre-historico"),
+        new PalavraDica("albatroz", "ave procelariidia"),
+        new PalavraDica("yugoslavia", "pais"),
+        new PalavraDica("uzbequistao", "ex-republica sovietica da asia central"),
+        new PalavraDica("artico", "norte"),
+        new PalavraDica("antartica", "sul"),
+        new PalavraDica("vida social", "nao tem quando se estuda na UTF"),
+        new PalavraDica("algema", "prendedor"),
+        new PalavraDica("aerodromo", "aviao"),
+    };
+}
---
+++ src/projetosd/PalavraDica.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package projetosd;
+
+/**
+ *
+ * @author jennifer
+ */
+public class PalavraDica {
+    private final String palavra;
+    private final String dica;
+
+    public PalavraDica(String palavra, String dica) {
+        this.palavra = palavra;
+        this.dica = dica;
+    }
+
+    public String getPalavra() {
+        return palavra;
+    }
+
+    public String getDica() {
+        return dica;
+    }
+    
+    
+}
